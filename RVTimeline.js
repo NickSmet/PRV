ecf60d995d783d97bb180e0408908c1c255cb43a
Nicksmet <nicksmet@live.com>
2020-07-03 12:16:02 +0300

    timeline add: kexts_not_loaded

--- RVTimeline.js
+++ RVTimeline.js
@@ -453,6 +453,8 @@ return result
 
 
     "PDFM_98595":{'regex':/Failed to setup bind-mode virtual netif/,"group":'VM_Network',"name":"PDFM-98595",'style':{'background-color':'rgb(230, 163, 186)'}},
+
+    "kexts_not_loaded":{'regex':/USB Connect Service not found\!/,"group":'VM_Errors',"name":"need kext-consent",'style':{'background-color':'rgb(234, 40, 40)'}},
   
 
   }
