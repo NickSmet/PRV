8558025b64c40a623d45271515c997e349953002
Nikolai Smetannikov <nicksmet@live.com>
2020-10-06 14:35:23 +0300

    vm.1.gz.log

--- RVTimeline.js
+++ RVTimeline.js
@@ -262,8 +262,6 @@ $("#restore").click(function(){
 
   window.addEventListener("load", function() {
     page = curr_url.match(url_regex) || '---'
-    console.log(curr_url.match(page)&&curr_groups)
-
     if (curr_url.match(page[0])&&curr_groups){
       var head = $("body");
       head.prepend('<p id=timelineGui><p>')
