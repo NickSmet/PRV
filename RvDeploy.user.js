639841f64aceeacb6f1825a66af2f88fb8d040f9
Nicksmet <nicksmet@live.com>
2021-05-21 20:18:44 +0300

    1.1.5.0 major de-spaghettization

--- RvDeploy.user.js
+++ RvDeploy.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Parallels ReportViewer 
-// @version            1.1.0.0
+// @version            1.1.5.0
 // @author 	Nikolai Smetannikov
 
 // @updateURL    https://github.com/NickSmet/PRV/raw/master/RvDeploy.user.js
