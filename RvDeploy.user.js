9d0bc57ca834471709ef26ffd530f5343bd3d00c
Nikolai Smetannikov <nicksmet@live.com>
2022-03-13 18:51:33 +0400

    1.2.2.3

--- RvDeploy.user.js
+++ RvDeploy.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Parallels ReportViewer 
-// @version            1.2.2.2
+// @version            1.2.2.3
 // @author 	Nikolai Smetannikov
 
 // @updateURL    https://github.com/NickSmet/PRV/raw/master/RvDeploy.user.js
