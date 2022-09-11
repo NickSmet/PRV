824296da42fdce3f43ef87a922ecf9ac7696e2a6
Nikolai Smetannikov <nicksmet@live.com>
2022-09-12 00:29:53 +0400

    1.4.3.2

--- ReportusVDeploy.user.js
+++ ReportusVDeploy.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Parallels Reportus Viewer 
-// @version            1.4.3.1
+// @version            1.4.3.2
 // @author 	Nikolai Smetannikov
 
 // @updateURL    https://github.com/NickSmet/PRV/raw/master/ReportusVDeploy.user.js
