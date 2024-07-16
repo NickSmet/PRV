bbfbfc7d586dd50a37499424233defea81ca5e83
Nikolai Smetannikov <nicksmet@live.com>
2024-07-16 16:36:35 +0400

    notification to not expect tools.log on non-Windows

--- NodeParser.js
+++ NodeParser.js
@@ -1228,6 +1228,11 @@ function parseTimeZone() {
 
 function parsetoolslog(item_all_data) {
     let toolsSuccess = false
+    console.log(bigReportObj.ParallelsProblemReport)
+    if (!bigReportObj.ParallelsProblemReport.GuestOs.match("Windows"))
+    { 
+            markBullet('tools.log','CustomHtml','<a title="Not Windows. Tools log not expected."> ℹ️ </a>')
+    }
 
     if(!item_all_data){return}
     //console.log(item_all_data);
