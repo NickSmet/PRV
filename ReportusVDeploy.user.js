ef95a083d43c1a118d6318b6a7296c940689f0aa
Nikolai Smetannikov <nicksmet@live.com>
2024-11-27 17:20:15 +0400

    fixed icons

--- ReportusVDeploy.user.js
+++ ReportusVDeploy.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     Parallels Reportus Viewer 
-// @version            1.6.6.0
+// @version            1.6.6.1
 // @author 	Nikolai Smetannikov
 
 // @updateURL    https://github.com/NickSmet/PRV/raw/master/ReportusVDeploy.user.js
